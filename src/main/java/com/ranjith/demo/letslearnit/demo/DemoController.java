03f586f569529032420e8352913d52d2619376f7
Ranjithkumar Mittapelly <ranjithkumarmittapelly@Ranjithkumars-Mac-mini.local>
2026-08-12 22:30:46 -0400

    Added  new endpoint greetings

--- src/main/java/com/ranjith/demo/letslearnit/demo/DemoController.java
+++ src/main/java/com/ranjith/demo/letslearnit/demo/DemoController.java
@@ -6,9 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    //comment to push
+
     @GetMapping("/getMeMyMsg")
     public String getMeMyData() {
         return "Thank you my Dear";
     }
+
+    @GetMapping("/getGreeting")
+    public String getGreeting() {
+        return "Hello! How are you ?";
+    }
+
 
 }
